55acf18b9322310e810cab6a0284d9c184f37bce
Pious <59928150+heypious@users.noreply.github.com>
2024-02-03 20:38:12 +0530

    Update question2.py

--- question2.py
+++ question2.py
@@ -3,7 +3,7 @@ import csv
 db = open('stu.csv', 'w')
 writer = csv.writer(db)
 to_add = eval(input('Enter [Roll No, Name, Age]: '))
-writer.writeline(to_add)
+writer.writerow(to_add) #writeline is not a function.
 db.close()
 with open('stu.csv', 'r') as db:
     reader = csv.reader(db)
