8f5e52cce01a619a1a2ac55011c02dad073a76be
Pious <59928150+heypious@users.noreply.github.com>
2024-02-03 20:46:31 +0530

    Update question5.py

--- question5.py
+++ question5.py
@@ -3,10 +3,11 @@ import mysql.connector as mq
 connection = mq.connect(host='10.0.0.2', user='root', password='t3po', db='prac')
 cursor = connection.cursor()
 cursor.execute('create table if not exists stock(item_no int(3) primary key, item_name varchar(20), rate int(5), quantity int(3), dos date);')
-
+cursor.commit() #executing commit function is required if any changes are made to the table
 # insert a values into table
 to_add = eval(input("(Item Number, Item Name, rate, quantity, Date of Sales): "))
 cursor.execute(f'insert into stock values({to_add})')
+cursor.commit()
 
 # display item number, name and amount.
 cursor.execute(f'select item_no, name, amount from stock;')
